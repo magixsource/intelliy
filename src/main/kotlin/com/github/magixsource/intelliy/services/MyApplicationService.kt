7017b071028737bb32cc9cc3db8003bbe090eb22
GitHub Action <action@github.com>
2021-08-27 02:05:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/magixsource/intelliy/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/magixsource/intelliy/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.magixsource.intelliy.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.magixsource.intelliy.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/magixsource/intelliy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/magixsource/intelliy/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.magixsource.intelliy.services
 
+import com.github.magixsource.intelliy.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
